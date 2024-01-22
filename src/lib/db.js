e238f4f10fe5bc91f98cc541efc37b313b664268
mjuhanne <mjuhanne@users.noreply.github.com>
2024-01-22 18:03:29 +0200

    Refactor Mangaupdates.com ratings

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,5 +5,19 @@ const response1 = await fetch('http://localhost:3300/json/admin.manga_data.json'
 const b = await response1.json()
 const response2 = await fetch('http://localhost:3300/json/ratings.json')
 const c = await response2.json()
-const admin={"manga_metadata":a,"manga_data":b,"ratings":c}
-export default admin
+
+let manga_titles = a['0'].manga_titles;
+    
+manga_titles.forEach(element => {
+    let id = element.enid;
+
+    if (id in c) {
+        element["rating_data"] = c[id]
+    } else {
+        element["rating_data"] = { "url":"http://mangaupdates.com", "rating":"N/A", "votes": 0, "last_updated":"N/A"}
+    }
+
+ });
+
+ const admin={"manga_metadata":a,"manga_data":b}
+ export default admin
